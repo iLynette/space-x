81288850c5573b2a1f147654656f71f2cc0ce42b
vikipretium <vighneshpasupathy@gmail.com>
2022-04-11 18:43:06 +0530

    add routes to all links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import Missions from './components/Missions/Missions';
+import Rockets from './components/Rockets/Rockets';
+import MyProfile from './components/MyProfile/MyProfile';
+import Dragons from './components/Dragons/Dragons';
 
-const App = () => <Navbar />;
+const App = () => {
+  <div className="app">
+    <Navbar />;
+    <Routes>
+      <Route path="/missions" element={<Missions />} />
+      <Route path="/rockets" element={<Rockets />} />
+      <Route path="/profile" element={<MyProfile />} />
+      <Route path="/dragons" element={<Dragons />} />
+    </Routes>
+  </div>;
+};
 
 export default App;
