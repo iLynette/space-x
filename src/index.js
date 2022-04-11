a948eea4977f4667db6a9f495b8030c67721ade5
vikipretium <vighneshpasupathy@gmail.com>
2022-04-11 20:47:29 +0530

    add router

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Router basename={process.env.PUBLIC_URL}>
-//       <App />
-//     </Router>
-//   </React.StrictMode>,
-//   document.getElementById('root'),
-// );
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
