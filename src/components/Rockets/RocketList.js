68c52f9eb09074abe830ec9cdb0d6de1b2d2fb5a
iLynette <lynetteacholah@icloud.com>
2022-04-11 22:24:02 +0300

    Fix linters

--- src/components/Rockets/RocketList.js
+++ src/components/Rockets/RocketList.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 function RocketList() {
   return (
     <div>RocketList</div>
-  )
+  );
 }
 
-export default RocketList
+export default RocketList;
