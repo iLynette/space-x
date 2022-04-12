79c81e45098b6c01a42ec2d462b120d99438dfe9
iLynette <lynetteacholah@icloud.com>
2022-04-12 17:41:39 +0300

    Import dispatch and define props

--- src/components/Rockets/Rocket.js
+++ src/components/Rockets/Rocket.js
@@ -1,12 +1,8 @@
-const Rocket = () => (
-  <>
-    <h1>Falcon 1</h1>
-    <p>
-      The Rockets section displays a list of all available SpaceX rockets.
-      Users can book each rocket by
-      clicking the reservation button or cancel the previously made booking
-    </p>
-    <button type="button" className="btn">Reserve Rocket</button>
-  </>
-);
-export default Rocket;
+import React from "react";
+import { useDispatch } from 'react-redux';
+
+const Rocket = (props) => {
+  const {
+    rocketId, rocketName, rocketDescription, rocketImage, rocketReserved,
+  } = props;
+}
