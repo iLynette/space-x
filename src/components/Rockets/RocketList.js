ca8730998b92b2496df05729a15bc744541aef65
iLynette <lynetteacholah@icloud.com>
2022-04-12 18:11:57 +0300

    Dispatch the rocket list

--- src/components/Rockets/RocketList.js
+++ src/components/Rockets/RocketList.js
@@ -3,3 +3,14 @@ import { useSelector, useDispatch } from 'react-redux';
 import { listRockets } from '../../redux/rockets/rockets';
 import Rocket from './Rocket';
 
+function RocketList() {
+  const dispatch = useDispatch();
+  const rocketsData = useSelector((state) => state.rocketReducer);
+  useEffect(() => {
+    if (rocketsData.length === 0) {
+      dispatch(listRockets());
+    }
+  });
+}
+
+export default RocketList;
