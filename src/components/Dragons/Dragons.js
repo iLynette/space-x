fb73e9c9dc590e1ceb260f802d98ca388d938e69
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-04-12 13:43:50 +0430

    basic structure for dragons

--- src/components/Dragons/Dragons.js
+++ src/components/Dragons/Dragons.js
@@ -1,2 +1,10 @@
-const Dragons = () => <div />;
-export default Dragons;
+export default function Dragons() {
+  return (
+    <>
+      <img src="https://img.icons8.com/ios/150/000000/the-dragon-team.png" alt="dragon" />
+      <h2>dragon name</h2>
+      <h2>dragon type</h2>
+      <button type="button">Reserve</button>
+    </>
+  );
+}
