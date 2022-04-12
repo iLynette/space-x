feb0587bc0a665768da9392cf02924f28c56d3e9
iLynette <lynetteacholah@icloud.com>
2022-04-12 17:43:45 +0300

    Add rocket files

--- src/components/Rockets/Rocket.js
+++ src/components/Rockets/Rocket.js
@@ -5,4 +5,17 @@ const Rocket = (props) => {
   const {
     rocketId, rocketName, rocketDescription, rocketImage, rocketReserved,
   } = props;
+
+  return (
+    <div className="rocket-container" key={rocketId}>
+      <div>
+        <img src={rocketImage} alt="/" />
+      </div>
+      <h1>{rocketName}</h1>
+      <p>
+        {rocketDescription}
+      </p>
+      <button type="button" className="btn" id={rocketReserved}>Reserve Rocket</button>
+    </div>
+  )
 }
