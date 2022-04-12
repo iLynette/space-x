05a8b3064c0a24e8d10c04e76835072d8026056b
iLynette <lynetteacholah@icloud.com>
2022-04-12 11:29:50 +0300

    Add rocket reducer

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,6 +1,21 @@
 const GET_ROCKETS = 'space-x/redux/rockets/LIST_ROCKETS';
 const rocketApi = 'https://api.spacexdata.com/v3/rockets';
 
+const defaultState = [];
+
+const rocketReducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case GET_ROCKETS:
+    return [
+      ...action.payload 
+    ];
+    default:
+      return state;
+  }
+}
+
+export default rocketReducer;
+
 
 
 
