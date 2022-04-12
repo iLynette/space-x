178810f4e3bbe6baf1f451553bdc1d0d8033ffad
iLynette <lynetteacholah@icloud.com>
2022-04-12 11:38:11 +0300

    Add rockets data

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -3,6 +3,11 @@ const rocketApi = 'https://api.spacexdata.com/v3/rockets';
 
 const defaultState = [];
 
+export const addRocket = (rockets) => ({
+  type: GET_ROCKETS,
+  payload: rockets,
+});
+
 const rocketReducer = (state = defaultState, action) => {
   switch(action.type) {
     case GET_ROCKETS:
