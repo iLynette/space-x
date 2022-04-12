7fa6e5eef0e53e0fe5c5d4bd84099d14064c933b
vikipretium <vighneshpasupathy@gmail.com>
2022-04-12 19:19:53 +0530

    add mission reducer in store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,14 +2,13 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rocketReducer from './rockets/rockets';
+import { missionReducer } from './missions/missions';
 
 const reducer = combineReducers({
   rocketReducer,
+  missionReducer,
 });
 
-const store = createStore(
-  reducer,
-  applyMiddleware(logger, thunk),
-);
+const store = createStore(reducer, applyMiddleware(logger, thunk));
 
 export default store;
