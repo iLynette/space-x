ed625468d9082ca16c4cb48f51bb70a1d5caa9fa
iLynette <lynetteacholah@icloud.com>
2022-04-12 11:56:32 +0300

    Fetch data

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,5 +1,4 @@
 const GET_ROCKETS = 'space-x/redux/rockets/LIST_ROCKETS';
-const rocketApi = 'https://api.spacexdata.com/v3/rockets';
 
 const defaultState = [];
 
@@ -7,6 +6,25 @@ export const addRocket = (rockets) => ({
   type: GET_ROCKETS,
   payload: rockets,
 });
+
+export const listRockets = () => async (dispatch) => {
+  try {
+      const fetchData = await fetch(
+        'https://api.spacexdata.com/v3/rockets',
+      );
+      const apiRockets = await fetchData.json();
+      dispatch({
+          type: GET_ROCKETS,
+          payload: apiRockets.map((rocket) => ({
+              rocketId: rocket.id,
+              rocketName: rocket_name,
+              rocketDescription: rocket_description,
+              rocketImage: rocket.flickr_images[0],
+              rocketReserved: false,
+          })),
+      })
+  }catch (error) {throw new Error(error)};
+}
 
 const rocketReducer = (state = defaultState, action) => {
   switch(action.type) {
