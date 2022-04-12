0e253496c165b32a237ae59bffc51d1fb0ee9c82
vikipretium <vighneshpasupathy@gmail.com>
2022-04-12 18:28:55 +0530

    add fetch api  using async and await

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -2,14 +2,14 @@ const FETCH_REQUEST = 'FETCH_REQUEST';
 const FETCH_SUCCESS = 'FETCH_SUCCESS';
 const FETCH_ERROR = 'FETCH_ERROR';
 
-const fetch_request = () => ({
+const fetchRequest = () => ({
   type: FETCH_REQUEST,
 });
-const fetch_success = () => ({
+const fetchSuccess = () => ({
   type: FETCH_SUCCESS,
   payload: missions,
 });
-const fetch_error = () => ({
+const fetchError = () => ({
   type: FETCH_ERROR,
   payload: error,
 });
@@ -20,30 +20,42 @@ const initialState = {
   error: '',
 };
 
-export const missionReducer = ((state = initialState), action) => {
-    switch (action.type)
-    {
-        case FETCH_REQUEST:
-            return {
-                ...state,
-                loading: true,
-            };
-        case FETCH_SUCCESS:
-            return{
-                ...state,
-                loading:false,
-                mission:action.payload,
-                error: '',
-            };
-        case FETCH_ERROR:
-            return {
-                ...state,
-                loading:false,
-                missions: [],
-                error: action.payload,
-            }
-        default:
-            return state;
-    }
-}
+export const missionReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        mission: action.payload,
+        error: '',
+      };
+    case FETCH_ERROR:
+      return {
+        ...state,
+        loading: false,
+        missions: [],
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
+const fetchMissions = () => async (dispatch) => {
+  dispatch(fetchRequest());
+  const request = await fetch('https://api.spacexdata.com/v3/missions');
+  try {
+    const missions = await request.json();
+    dispatch(fetchSuccess(missions));
+  } catch (error) {
+    const errorMsg = error.message;
+    dispatch(fetchError(errorMsg));
+  }
+};
+
+export default fetchMissions;
