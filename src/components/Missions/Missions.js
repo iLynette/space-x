bce40d15bc230f627c58a8a79bc2ad28bf103151
vikipretium <vighneshpasupathy@gmail.com>
2022-04-13 15:33:02 +0530

    add use selector and structure

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -1,10 +1,12 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import fetchMissions from '../../redux/missions/missions';
+import styles from './MissionStyle.css';
 
 let fetched = false;
 
 const Missions = () => {
+  const missions = useSelector((state) => state.missionReducer.missions);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -13,7 +15,40 @@ const Missions = () => {
       fetched = true;
     }
   }, []);
-  return <div />;
+
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th> </th>
+        </tr>
+        {missions.map(
+          ({
+            mission_name: missionName,
+            mission_id: missionId,
+            description,
+          }) => (
+            <tr key={missionId}>
+              <td className={styles['mission-name']}>{missionName}</td>
+              <td className={styles['mission-description']}>{description}</td>
+              <td className={styles['mission-status']}>
+                {' '}
+                <p className={styles['not-reserved']}>NOT A MEMBER</p>
+              </td>
+              <td>
+                <button className={styles['mission-not-join']} type="button">
+                  Join Mission
+                </button>
+              </td>
+            </tr>
+          ),
+        )}
+      </tbody>
+    </table>
+  );
 };
 
 export default Missions;
