f85d08fc804a917f2b6ece9108eebe28f6c46e50
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-04-14 00:35:52 +0430

    profile reservation added

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -1,24 +1,40 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function MyProfile() {
+  const dragons = useSelector((state) => state.dragonsReducer);
+
   return (
     <div className="profileBody">
       <div>
         <h2 className="">My Missions</h2>
         <div className="profileMissions">
           {/* list of missions */}
+          {/* <img src="https://img.icons8.com/external-tone-royyan-wijaya/64/000000/external-flying-space-duotone-tone-royyan-wijaya.png"/> */}
         </div>
       </div>
       <div>
         <h2 className="">My Rockets</h2>
         <div className="profileRockets">
           {/* list of rockets */}
+          {/* <img src="https://img.icons8.com/external-xnimrodx-blue-xnimrodx/64/000000/external-rocket-transport-xnimrodx-blue-xnimrodx.png"/> */}
         </div>
       </div>
       <div>
         <h2 className="">My Dragons</h2>
         <div className="profileDragons">
-          {/* list of dragons */}
+          {
+          dragons.map((dragon) => (
+            <>
+              {dragon.reserved && (
+                <div key={dragon.id} className="profileCard">
+                  <img src="https://img.icons8.com/external-xnimrodx-blue-xnimrodx/64/000000/external-rocket-online-marketing-xnimrodx-blue-xnimrodx.png" alt="reserved" />
+                  <h3>{dragon.name}</h3>
+                </div>
+              )}
+            </>
+          ))
+          }
         </div>
       </div>
     </div>
