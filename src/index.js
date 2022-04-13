e24666cd3b1e07e21b605993143a8e99b881585a
vikipretium <vighneshpasupathy@gmail.com>
2022-04-13 15:32:48 +0530

    add store and provider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/ConfigureStore';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,5 +12,5 @@ ReactDOM.render(
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
