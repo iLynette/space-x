3145c7b62ae9fbd62f24229e47d3b8ccc309613b
iLynette <lynetteacholah@icloud.com>
2022-04-13 09:54:17 +0300

    Add the toggle reserve case to reducer

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -28,9 +28,19 @@ export const listRockets = () => async (dispatch) => {
 const rocketReducer = (state = defaultState, action) => {
   switch (action.type) {
     case GET_ROCKETS:
-      return [
+      { return [
         ...action.payload,
-      ];
+      ]; }
+      case TOGGLE_RESERVE: {
+        const updatedState = state.map((rocket) => {
+          if (rocket.rocketId.toString() !== action.payload.id) {
+            console.log(rocket.rocketId)
+            return rocket;
+          }
+          return { ...rocket, rocketReserved: !rocket.rocketReserved };
+        });
+        return [...updatedState];
+      }
     default:
       return state;
   }
