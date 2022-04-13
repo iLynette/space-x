0ff57bd98adb6865d3252e90ca48c76e26183b80
iLynette <lynetteacholah@icloud.com>
2022-04-13 11:41:55 +0300

    Import usedispatch and the togglereserve function

--- src/components/Rockets/Rocket.js
+++ src/components/Rockets/Rocket.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { toggleReserve } from '../../redux/rockets/rockets';
 
 const Rocket = (props) => {
   const {
