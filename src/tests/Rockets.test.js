a7f52979ffcf35cfceccf4cd41e9f384df782055
iLynette <lynetteacholah@icloud.com>
2022-04-14 19:43:54 +0300

    Adjust imports and test the store

--- src/tests/Rockets.test.js
+++ src/tests/Rockets.test.js
@@ -2,16 +2,31 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import store from '../redux/configureStore';
-import Rockets, { Rocket } from '../components/Dragons/Dragons';
+import Rockets from '../components/Dragons/Dragons';
+import RocketList from '../components/Rockets/RocketList';
 
-describe('Dragon tests', () => {
+describe('Rocket tests', () => {
     it('render test', () => {
       const rocket = {
         id: 1,
         image: 'https://img.icons8.com/external-xnimrodx-blue-xnimrodx/64/000000/external-rocket-online-marketing-xnimrodx-blue-xnimrodx.png',
-        name: 'dragon1',
+        name: 'falcon1',
         description: 'description for dragon1',
         reserved: false,
       };
+
+      const tree = renderer.create(
+        <Provider store={store}>
+          <RocketList
+            id={rocket.id}
+            image={rocket.image}
+            name={rocket.name}
+            type={rocket.type}
+            description={rocket.description}
+            reserved={rocket.reserved}
+          />
+        </Provider>,
+      );
+      expect(tree).toMatchSnapshot();
     });
   });
