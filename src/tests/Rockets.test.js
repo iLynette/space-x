63007ccdeaeabf3ea2b1d76c95924beff55ecb66
iLynette <lynetteacholah@icloud.com>
2022-04-14 19:25:15 +0300

    Import test files

---
+++ src/tests/Rockets.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import store from '../redux/configureStore';
+import Rockets, { Rocket } from '../components/Dragons/Dragons';
