1910a4ddf2ff53b115087aa4a6048f587d6853f9
iLynette <lynetteacholah@icloud.com>
2022-04-14 20:04:31 +0300

    Fix linters

--- src/tests/Rockets.test.js
+++ src/tests/Rockets.test.js
@@ -6,36 +6,36 @@ import Rockets from '../components/Dragons/Dragons';
 import RocketList from '../components/Rockets/RocketList';
 
 describe('Rocket tests', () => {
-    it('render test', () => {
-      const rocket = {
-        id: 1,
-        image: 'https://img.icons8.com/external-xnimrodx-blue-xnimrodx/64/000000/external-rocket-online-marketing-xnimrodx-blue-xnimrodx.png',
-        name: 'falcon1',
-        description: 'description for dragon1',
-        reserved: false,
-      };
+  it('render test', () => {
+    const rocket = {
+      id: 1,
+      image: 'https://img.icons8.com/external-xnimrodx-blue-xnimrodx/64/000000/external-rocket-online-marketing-xnimrodx-blue-xnimrodx.png',
+      name: 'falcon1',
+      description: 'description for dragon1',
+      reserved: false,
+    };
 
-      const tree = renderer.create(
-        <Provider store={store}>
-          <RocketList
-            id={rocket.id}
-            image={rocket.image}
-            name={rocket.name}
-            type={rocket.type}
-            description={rocket.description}
-            reserved={rocket.reserved}
-          />
-        </Provider>,
-      );
-      expect(tree).toMatchSnapshot();
-    });
-
-    it('Test if rockets render correctly', () => {
-        const tree = renderer.create(
-          <Provider store={store}>
-            <Rockets />
-          </Provider>,
-        );
-        expect(tree).toMatchSnapshot();
-      });
+    const tree = renderer.create(
+      <Provider store={store}>
+        <RocketList
+          id={rocket.id}
+          image={rocket.image}
+          name={rocket.name}
+          type={rocket.type}
+          description={rocket.description}
+          reserved={rocket.reserved}
+        />
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
   });
+
+  it('Test if rockets render correctly', () => {
+    const tree = renderer.create(
+      <Provider store={store}>
+        <Rockets />
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
