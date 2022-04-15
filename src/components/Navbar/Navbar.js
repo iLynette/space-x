f7f296a2f4e653ac126c6bcff86237925b189cff
vikipretium <vighneshpasupathy@gmail.com>
2022-04-15 19:53:48 +0530

    add logo effect

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from '../../images/logoBW.png';
+import logoBW from '../../images/logoBW.png';
+import logoCL from '../../images/logoCLB.png';
 import styles from './Navbar.module.css';
 
 const link = [
@@ -26,28 +27,39 @@ const link = [
   },
 ];
 
-const Navbar = () => (
-  <nav className={styles.container}>
-    <ul className={styles.list}>
-      <li className={styles.topic}>
-        <img src={logo} className={styles.logo} alt="travelers site logo" />
-        <h1 style={{ fontSize: '2.1rem' }}>SPACE X</h1>
-      </li>
-      {link.map(({ id, path, text }) => (
-        <li key={id}>
-          <NavLink
-            to={path}
-            className="nav-link"
-            style={({ isActive }) => ({
-              textDecoration: isActive ? 'underline' : 'none',
-              color: '#0079f9',
-            })}
-          >
-            {text}
-          </NavLink>
+const Navbar = () => {
+  const [colored, setColored] = useState(false);
+  return (
+    <nav className={styles.container}>
+      <ul className={styles.list}>
+        <li className={styles.topic}>
+          <img
+            src={colored ? logoCL : logoBW}
+            className={styles.logo}
+            alt="travelers site logo"
+            onMouseOver={() => setColored(!colored)}
+            onMouseOut={() => setColored(!colored)}
+            onFocus={() => 0}
+            onBlur={() => 0}
+          />
+          <h1 style={{ fontSize: '2.1rem' }}>SPACE X</h1>
         </li>
-      ))}
-    </ul>
-  </nav>
-);
+        {link.map(({ id, path, text }) => (
+          <li key={id}>
+            <NavLink
+              to={path}
+              className="nav-link"
+              style={({ isActive }) => ({
+                textDecoration: isActive ? 'underline' : 'none',
+                color: '#0079f9',
+              })}
+            >
+              {text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 export default Navbar;
