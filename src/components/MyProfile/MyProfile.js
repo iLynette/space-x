b1ba8064828052ef92093fd500569760a92100fc
vikipretium <vighneshpasupathy@gmail.com>
2022-04-15 06:47:27 +0530

    add missions to profile

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -5,32 +5,48 @@ export default function MyProfile() {
   const dragons = useSelector((state) => state.dragonsReducer);
   const rockets = useSelector((state) => state.rocketReducer);
   const reservedRockets = rockets.filter((rocket) => rocket.rocketReserved);
-
+  const reservedMission = useSelector((state) => state.missionReducer.mission.filter(
+    (missionItem) => missionItem.reserved === true,
+  ));
   return (
     <div className="profileBody">
       <div>
         <h2 className="">My Missions</h2>
         <div className="profileMissions">
           {/* list of missions */}
-          {/* <img src="https://img.icons8.com/external-tone-royyan-wijaya/64/000000/external-flying-space-duotone-tone-royyan-wijaya.png"/> */}
+          <ul className="reserve-mission">
+            {reservedMission.length === 0 ? (
+              <li className="no-reserved-mission">No Mission Reserved</li>
+            ) : (
+              reservedMission.map((mission) => (
+                <li key={mission.mission_id} className="reserved-item">
+                  <img
+                    src="https://img.icons8.com/external-xnimrodx-blue-xnimrodx/64/000000/external-rocket-online-marketing-xnimrodx-blue-xnimrodx.png"
+                    alt="reserved"
+                  />
+                  <span className="name">{mission.mission_name}</span>
+                </li>
+              ))
+            )}
+          </ul>
         </div>
       </div>
       <div>
         <h2 className="">My Rockets</h2>
         <div className="profileRockets">
           <ul className="reserve-rocket">
-            {(reservedRockets.length === 0) ? (
-              <li className="no-reserved-rocket">
-                No Rockets Reserved
-              </li>
+            {reservedRockets.length === 0 ? (
+              <li className="no-reserved-rocket">No Rockets Reserved</li>
             ) : (
-              reservedRockets
-                .map((rocket) => (
-                  <li key={rocket.rocketId} className="reserved-item">
-                    <img src="https://img.icons8.com/external-xnimrodx-blue-xnimrodx/64/000000/external-rocket-online-marketing-xnimrodx-blue-xnimrodx.png" alt="reserved" />
-                    <span className="name">{rocket.rocketName}</span>
-                  </li>
-                ))
+              reservedRockets.map((rocket) => (
+                <li key={rocket.rocketId} className="reserved-item">
+                  <img
+                    src="https://img.icons8.com/external-xnimrodx-blue-xnimrodx/64/000000/external-rocket-online-marketing-xnimrodx-blue-xnimrodx.png"
+                    alt="reserved"
+                  />
+                  <span className="name">{rocket.rocketName}</span>
+                </li>
+              ))
             )}
           </ul>
         </div>
@@ -49,7 +65,10 @@ export default function MyProfile() {
                   .filter(({ reserved }) => reserved === true)
                   .map((dragon) => (
                     <li key={dragon.id} className="profileCard">
-                      <img src="https://img.icons8.com/external-xnimrodx-blue-xnimrodx/64/000000/external-rocket-online-marketing-xnimrodx-blue-xnimrodx.png" alt="reserved" />
+                      <img
+                        src="https://img.icons8.com/external-xnimrodx-blue-xnimrodx/64/000000/external-rocket-online-marketing-xnimrodx-blue-xnimrodx.png"
+                        alt="reserved"
+                      />
                       <h3>{dragon.name}</h3>
                     </li>
                   ))
