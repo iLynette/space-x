15eb12cafea599cb2fa05f693afd37027fdbaeec
iLynette <lynetteacholah@icloud.com>
2022-04-16 08:44:25 +0300

    Use conditional rendering when rockets are not reserved

--- src/components/Rockets/Rocket.js
+++ src/components/Rockets/Rocket.js
@@ -30,7 +30,7 @@ const Rocket = (props) => {
           {rocketDescription}
         </p>
         <div className="reserve-rocket">
-          {rocketReserved ? (<button type="button" onClick={handleClick} className="btn cancel">Cancel Reservation</button>) : (<button type="button" className="btn" onClick={handleClick}>Reserve Rocket</button>)}
+          {!rocketReserved ? (<button type="button" className="btn" onClick={handleClick}>Reserve Rocket</button>) : (<button type="button" onClick={handleClick} className="btn cancel">Cancel Reservation</button>)}
         </div>
       </div>
     </div>
